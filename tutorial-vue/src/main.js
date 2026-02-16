dd02563906fa7858c6a2a61af13ceacfd77651bd
Javier <javier.rubior@estudiante.uam.es>
2026-02-16 13:51:51 +0100

    pdf 2.1 punto 1.5

---
+++ tutorial-vue/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+app.config.devtools = true
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
+
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
