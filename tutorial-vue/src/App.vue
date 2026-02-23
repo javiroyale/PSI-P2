84a2fc635e984de7d8fc400a825bfd0ea20a09fd
Sandra <ssndra2005@gmail.com>
2026-02-23 13:48:24 +0100

    listo para testing

--- tutorial-vue/src/App.vue
+++ tutorial-vue/src/App.vue
@@ -9,43 +9,72 @@
     <div class="row">
       <div class="col-md-12">
         <!-- Inclusion del componente "TablaPersonas" -->
-        <tabla-personas :personas="personas" /> <!-- Añadido para transferir datos-->
+        <formulario-persona @add-persona="agregarPersona" />
+        <tabla-personas :personas="personas" @delete-persona="eliminarPersona" @actualizar-persona="actualizarPersona" />
       </div>
     </div>
   </div>
 </template>
 <script setup>
-  // Importacion del componente "TablaPersonas"
-  import TablaPersonas from '@/components/TablaPersonas.vue'
-  import { ref } from 'vue';
+// Importacion del componente "TablaPersonas"
+import TablaPersonas from '@/components/TablaPersonas.vue'
+import FormularioPersona from '@/components/FormularioPersona.vue'
+import { ref } from 'vue';
 
-    // Declaracion de una variable reactiva "personas" usando "ref"
-  const personas = ref([
-    {
-      id: 1,
-      nombre: 'Jon',
-      apellido: 'Nieve',
-      email: 'jon@email.com',
-    },
-    {
-      id: 2,
-      nombre: 'Tyrion',
-      apellido: 'Lannister',
-      email: 'tyrion@email.com',
-    },
-    {
-      id: 3,
-      nombre: 'Daenerys',
-      apellido: 'Targaryen',
-      email: 'daenerys@email.com',
-    },
-  ]);
+const personas = ref([{
+  id: 1,
+  nombre: 'Jon',
+  apellido: 'Nieve',
+  email: 'jon@email.com',
+},
+{
+  id: 2,
+  nombre: 'Tyrion',
+  apellido: 'Lannister',
+  email: 'tyrion@email.com',
+},
+{
+  id: 3,
+  nombre: 'Daenerys',
+  apellido: 'Targaryen',
+  email: 'daenerys@email.com',
+},
+]);
+// definimos una funcion de nombre agregarPersona que agrega una nueva,→ persona al array
+const agregarPersona = (persona) => {
+  let id = 0;
+  if (personas.value.length > 0) {
+    id = personas.value[personas.value.length - 1].id + 1;
+  }
+  personas.value = [...personas.value, { ...persona, id }];
+};
+
+const eliminarPersona = (id) => {
+  try {
+    personas.value = personas.value.filter(
+      u => u.id !== id
+    );
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+
+const actualizarPersona = (id, personaActualizada) => {
+  try {
+    personas.value = personas.value.map(persona =>
+      persona.id === id ? personaActualizada : persona);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
 
 </script>
 <style>
-  /* Estilos globales para todos los elementos button en la aplicacion */
-  button {
+/* Estilos globales para todos los elementos button en la aplicacion */
+button {
   background: #009435;
   border: 1px solid #009435;
-  }
+}
 </style>
--- tutorial-vue/src/components/TablaPersonas.vue
+++ tutorial-vue/src/components/TablaPersonas.vue
@@ -4,46 +4,99 @@
   <!-- Contenedor principal del componente -->
   <div id="tabla-personas">
     <!-- Tabla para mostrar informacion de personas -->
-    <table class="table">
-      <!-- Encabezado de la tabla -->
-      <thead>
-        <tr>
-          <!-- Columnas del encabezado -->
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <!-- Cuerpo de la tabla -->
-      <tbody>
-        <tr v-for="persona in personas" :key="persona.id">
-          <!-- Celda de datos para el nombre de la persona -->
-          <td>{{ persona.nombre }}</td>
-          <!-- Celda de datos para el apellido de la persona -->
-          <td>{{ persona.apellido }}</td>
-          <!-- Celda de datos para el correo electronico de la persona -->
-          <td>{{ persona.email }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="!personas.length" class="alert alert-info" role="alert">
+      No se han encontrado personas
+    </div>
+    <div v-else>
+      <table class="table">
+        <!-- Encabezado de la tabla -->
+        <thead>
+          <tr>
+            <!-- Columnas del encabezado -->
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <!-- Cuerpo de la tabla -->
+        <tbody>
+          <tr v-for="persona in personas" :key="persona.id">
+            <td v-if="editando === persona.id">
+              <input id="persona.nombre" v-model="persona.nombre" type="text" class="form-control"
+                data-cy="persona-nombre">
+            </td>
+            <td v-else>
+              {{ persona.nombre }}
+            </td>
+            <td v-if="editando === persona.id">
+              <input v-model="persona.apellido" type="text" class="form-control">
+            </td>
+            <td v-else>
+              {{ persona.apellido }}
+            </td>
+            <td v-if="editando === persona.id">
+              <input v-model="persona.email" type="email" class="form-control">
+            </td>
+            <td v-else>
+              {{ persona.email }}
+            </td>
+            <td v-if="editando === persona.id">
+              <button class="btn btn-success" data-cy="save-button" @click="guardarPersona(persona)">&#x1F5AB;
+                Guardar</button>
+              <button class="btn btn-secondary ml-2" data-cy="cancel-button" @click="cancelarEdicion(persona)">&#x1F5D9;
+                Cancelar
+              </button>
+            </td>
+            <td v-else>
+              <button class="btn btn-info" data-cy="edit-button" @click=" editarPersona(persona)">&#x1F58A; Editar
+              </button>
+              <button class="btn btn-danger ml-2" data-cy="delete-button"
+                @click="$emit('delete-persona', persona.id)">&#x1F5D1; Eliminar
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 // definicion del componente
 defineOptions({
-// nombre del componente
+  // nombre del componente
   name: 'tabla-personas',
 });
 // declaramos y damos valor por defecto para la propiedad personas
 const props = defineProps({
-  personas: {type: Array, default: () => []},
+  personas: { type: Array, default: () => [] },
 });
+
+const emit = defineEmits(['actualizar-persona']);
+const editando = ref(null);
+const personaEditada = ref(null);
+const editarPersona = (persona) => {
+  personaEditada.value = { ...persona };
+  editando.value = persona.id;
+};
+
+const guardarPersona = (persona) => {
+  if (!persona.nombre.length || !persona.apellido.length || !persona.email.length) {
+    return;
+  }
+  emit('actualizar-persona', persona.id, persona);
+  editando.value = null;
+};
+
+const cancelarEdicion = (persona) => {
+  Object.assign(persona, personaEditada.value);
+  editando.value = null;
+};
 
 </script>
 
 <style scoped>
-  /* Estilos especificos del componente con el modificador "scoped" */
+/* Estilos especificos del componente con el modificador "scoped" */
 </style>
-
-
